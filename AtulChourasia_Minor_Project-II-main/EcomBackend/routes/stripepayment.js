48960872b64b3d5d87a74ec6bde154507478d857
AtulChourasia <67797718+AtulChourasia@users.noreply.github.com>
2022-04-26 07:00:12 +0530

    .
    .

--- AtulChourasia_Minor_Project-II-main/EcomBackend/routes/stripepayment.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { isSignedIn, isAuthenticated } = require("../controllers/auth");
-const { makePayment } = require("../controllers/stripepayment");
-router.post("/stripepayment", makePayment);
-
-module.exports = router;
